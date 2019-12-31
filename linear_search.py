1449cb0be170932a3f0ac0908f0650efab610109
Sumit Jaswal <57674054+sumit-jaswal@users.noreply.github.com>
2019-12-31 20:10:04 +0530

    Add files via upload

---
+++ linear_search.py
@@ -0,0 +1,22 @@
+# Lenear Search - Searching for a element 'search' one by one by comparing it with each element in the list 'lis'
+def linear_search(l,num):
+	for i in range(len(l)):
+		if(l[i]==num):
+			found = True
+			break
+		else:
+			found = False
+	if(found is True):
+		return True
+	else:
+		return False
+			
+	# Pythonic way to search an element
+	'''if(search in lis):
+		return True	
+	else:
+		return False'''	
+
+lis = [8,4,78,232,89,213]
+search = 213
+print(linear_search(lis,search))
